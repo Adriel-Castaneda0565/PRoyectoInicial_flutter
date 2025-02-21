4576f387d560182e22efc71b37dc540fdadc4a06
Adriel-Castaneda0565 <164534487+Adriel-Castaneda0565@users.noreply.github.com>
2025-02-21 10:53:48 -0600

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MiAppBar());
+}//fin de main
+
+
+class MiAppBar extends StatelessWidget {
+  const MiAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title:Text('Hola mi AppBar',style: TextStyle(
+                color: Colors.white, // Color de la letra
+              ),),
+          centerTitle: true,
+          backgroundColor:Colors.indigo,
+          leading: IconButton(
+            icon: Icon(Icons.menu), // Ícono de leading
+            onPressed: () {
+              // Acción al presionar el ícono
+            },
+            color:Colors.white,//color de fondo 
+          ),
+        ),
+      ),
+    );
+  }//fin de build
+}//fin clase MiAppBar 
